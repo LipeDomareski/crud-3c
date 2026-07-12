db1b0bdd3145bd83aa9ba46af9966dad53cfb9d1
lipe <domareskifelipe44@gmail.com>
2026-07-12 16:35:59 -0300

    fix: allow frontend local network origin

--- backend/src/config/config.php
+++ backend/src/config/config.php
@@ -2,10 +2,22 @@
 
     $dataFile = __DIR__ . '/../../data/data.json';
 
-    $allowedOrigins =[
-        'http://0.0.0.0:8080', 'http://localhost:8080', 'http://127.0.0.1:8080',
-        'http://0.0.0.0:5500', 'http://localhost:5500', 'http://127.0.0.1:5500',
-    ];
+        $allowedOrigins = [
+            'http://0.0.0.0:8080',
+            'http://localhost:8080',
+            'http://127.0.0.1:8080',
+            'http://192.168.3.123:8080',
+
+            'http://0.0.0.0:5500',
+            'http://localhost:5500',
+            'http://127.0.0.1:5500',
+        ];
 
 
 ?>
+
+
+    <!-- $allowedOrigins =[
+        'http://0.0.0.0:8080', 'http://localhost:8080', 'http://127.0.0.1:8080',
+        'http://0.0.0.0:5500', 'http://localhost:5500', 'http://127.0.0.1:5500',
+    ]; -->
